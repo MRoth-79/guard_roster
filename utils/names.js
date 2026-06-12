a407921b3a5dea7c9f0a17ed6199dbefc8c3689d
MRoth-79 <maorot@gmail.com>
2026-06-13 02:24:02 +0300

    Add functions for employee name handling

--- utils/names.js
+++ utils/names.js
@@ -1,1 +1,24 @@
+export function allEmployeeNames() {
+  return Object.keys(this.C.COLOR_MAP)
+    .map((name) => name.replace(/_/g, " "))
+    .sort((a, b) => a.localeCompare(b, "he"));
+}
+
+export function getScheduledEmployeeNames(allShifts) {
+  return Object.keys(allShifts)
+    .filter((name) => (allShifts[name] || 0) > 0)
+    .sort((a, b) => {
+      const aCount = allShifts[a] || 0;
+      const bCount = allShifts[b] || 0;
+      if (bCount !== aCount) return bCount - aCount;
+      return a.localeCompare(b, "he");
+    });
+}
+
+export function nameToColorClass(name) {
+  const clean = this.normalizeKey(name);
+  if (this.C.COLOR_MAP[clean]) return this.C.COLOR_MAP[clean];
+  const underscored = clean.replace(/\s+/g, "_");
+  return this.C.COLOR_MAP[underscored] || null;
+}
 
