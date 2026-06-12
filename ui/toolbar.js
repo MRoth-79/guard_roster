92d8933eec946cb1e70900eebf8d7c8a149de498
MRoth-79 <maorot@gmail.com>
2026-06-13 02:22:42 +0300

    Add bindToolbar function to handle URL toggle

--- ui/toolbar.js
+++ ui/toolbar.js
@@ -1,1 +1,7 @@
+export function bindToolbar() {
+  this.el.toggleUrlButton.addEventListener("click", () => {
+    const open = this.el.urlInputWrap.classList.toggle("open");
+    this.el.toggleUrlButton.textContent = open ? "🔗 סגור URL" : "🔗 URL";
+  });
+}
 
