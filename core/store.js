df2fe036ecac1e95b1184db4d233f98b542a9905
MRoth-79 <maorot@gmail.com>
2026-06-13 02:12:19 +0300

    Refactor store to createStore function with initial state

--- core/store.js
+++ core/store.js
@@ -1,25 +1,23 @@
-#core/store.js
-let state = {
-  matrix: [],
-  startDate: "",
-  lockedName: null,
-};
-
-const listeners = [];
-
-export function initStore() {
-  state.matrix = [];
-}
-
-export function getState() {
-  return state;
-}
-
-export function setState(update) {
-  state = { ...state, ...update };
-  listeners.forEach(fn => fn(state));
-}
-
-export function subscribe(fn) {
-  listeners.push(fn);
+export function createStore(initialState = {}) {
+  return {
+    state: {
+      excelMatrix: [],
+      lockedName: null,
+      searchQuery: "",
+      parsedData: null,
+      startDate: "",
+      ...initialState,
+    },
+    listeners: [],
+    setState(update) {
+      this.state = { ...this.state, ...update };
+      this.listeners.forEach((fn) => fn(this.state));
+    },
+    subscribe(fn) {
+      this.listeners.push(fn);
+    },
+    getState() {
+      return this.state;
+    },
+  };
 }
