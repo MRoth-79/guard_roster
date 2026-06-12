680b79d3672dc894a66cd07ae2f189ceda4aabc1
MRoth-79 <maorot@gmail.com>
2026-06-13 01:24:53 +0300

    Implement state management with localStorage
    Add functions to save and restore application state using localStorage.

--- core/state-sync.js
+++ core/state-sync.js
@@ -1,1 +1,17 @@
+#core/state-sync.js
+import { setState } from "./store.js";
+import { STORAGE_KEYS } from "./constants.js";
+
+export function restoreState() {
+  try {
+    const data = localStorage.getItem(STORAGE_KEYS.FULL_STATE);
+    if (data) {
+      setState(JSON.parse(data));
+    }
+  } catch {}
+}
+
+export function saveState(state) {
+  localStorage.setItem(STORAGE_KEYS.FULL_STATE, JSON.stringify(state));
+}
 
